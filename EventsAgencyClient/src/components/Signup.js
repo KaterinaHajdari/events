40598bc7a1fd457a6f87d74c235ddadb8cadb20f
katerina hajdari <k.hajdari@yottaengineering.com>
2020-08-12 11:56:59 +0200

    fixes

--- EventsAgencyClient/src/components/Signup.js
+++ EventsAgencyClient/src/components/Signup.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import '/home/x.lici/Desktop/eventsagency/EventsAgencyClient/src/components/Signup.css';
+import './Signup.css';
 class Signup extends React.Component{
     render(){
         return(
