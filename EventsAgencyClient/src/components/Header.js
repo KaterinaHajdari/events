aae4612f99de361bf3cf864545cbf03e318c33c7
xheni <x.lici@yottaengineering.comgit config --global color.ui autogit config --global merge.conflictstyle diff3git config --listqquitexit>
2020-08-12 10:04:52 +0200

    Login Layout

--- EventsAgencyClient/src/components/Header.js
+++ EventsAgencyClient/src/components/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link, Route } from "react-router-dom";
 import About from "./About";
-import { Navbar, Button } from "reactstrap";
+import Login from './Login';
+import { Navbar} from "reactstrap";
 
 class Header extends React.Component {
   render() {
@@ -25,11 +26,10 @@ class Header extends React.Component {
         <Navbar style={navbarStyle}>
           <Link to="/">HOME</Link>
           <Link to="/about">About</Link>
-          
-          <Button style={buttonStyle}>LOGIN</Button>{" "}
+         <Link to="/login" style={buttonStyle}>LOGIN</Link>
         </Navbar>
         <Route path="/about" component={About} />
-       
+        <Route path="/login" component={Login} />
       </div>
     );
   }
