3616b76e08a8cf1704c683bf084284effc6d7416
katerina hajdari <k.hajdari@yottaengineering.com>
2020-08-19 17:09:34 +0200

    changes

--- EventsAgencyClient/src/components/Profile.js
+++ EventsAgencyClient/src/components/Profile.js
@@ -1,30 +1,55 @@
 import React from "react";
 import avatar from "../avatar.png";
-import {Link} from 'react-router-dom';
-import {
-    Container,
-    Row,
-    Col
-   
-    
-} from 'react-bootstrap';
-import '../css/Profile.css'
-import '../css/Header.css'
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import "../css/Profile.css";
+import "../css/Header.css";
 import { Button } from "reactstrap";
-class Profile extends React.Component{
+class Profile extends React.Component {
+  render() {
+    return (
+      <div className="profile-container">
+        <Container>
 
-render(){
-    return(<div>
-  <Container className="profile-container">
-      <Row>
-      <Col md={6}>
-      <img src={avatar}  className="mx-auto d-block" id="avatar"/>
-    </Col>
-    
-      </Row>
-      <Button id="edit-btn"><Link to='/editProfile'>Edit</Link></Button>
-  </Container>
-    </div>)
+          <Row>
+            <Col md={6}>
+              <img src={avatar} id="avatar" />
+            </Col>
+          </Row>
+          <div className="contain">
+          <Row xs={2} md={4} lg={6}>
+            <Col>
+              <Col>username:</Col>
+              
+            </Col>
+            <Col>
+              <Col>Admin</Col>
+              
+            </Col>
+          </Row>
+
+          <Row xs={2} md={4} lg={6}>
+            <Col>
+              <Col>email:</Col>
+              
+            </Col>
+            <Col>
+              <Col>admin@mail.com</Col>
+              
+            </Col>
+          </Row>
+          </div>
+          
+          <Row>
+            <Col>
+              <Button id="edit-btn">
+                <Link to="/editProfile">Edit</Link>
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-}
-export default Profile
+export default Profile;
