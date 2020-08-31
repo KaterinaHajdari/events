8bfeafb8e064c9c54e160318a55d608404d481c1
xheni <x.lici@yottaengineering.comgit config --global color.ui autogit config --global merge.conflictstyle diff3git config --listqquitexit>
2020-08-31 14:25:10 +0200

    Changes

---
+++ EventsAgencyClient/src/redux/actions/CreateEvent.js
@@ -0,0 +1,15 @@
+import users from '../apis/events';
+
+export const createEventReservation = eventValues => async dispatch => {
+    
+    dispatch({type: "EVENT_BEGIN"});
+
+    const response = await users.post("/users", eventValues)
+    .then(res => {
+        dispatch({type: "EVENT_SUCCESS",  payload: eventValues});
+    })
+    .catch(err => {
+        dispatch({type: "EVENT_FAILURE", payload: err});
+    })
+
+}
