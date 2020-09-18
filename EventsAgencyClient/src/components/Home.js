c212368a21ccbdb0b1b2cf9b642b53bda662ae37
katerina hajdari <k.hajdari@yottaengineering.com>
2020-09-18 17:29:46 +0200

    user component

--- EventsAgencyClient/src/components/Home.js
+++ EventsAgencyClient/src/components/Home.js
@@ -1,63 +1,57 @@
-import React from 'react';
+import React from "react";
 
 import HomeImage from "./images/wedding1.jpg";
 import Birthday from "./images/Birthday.png";
 import Home3 from "./images/Home3.png";
-import Home1 from "./images/Home1.png"
+import Home1 from "./images/Home1.png";
 import {
-    Container,
-    Row,
-    Col,
-    Button,
-    Card, 
-    CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle
-} from 'reactstrap';
-export default()=>{
-    return( 
-<div>
-  
-   
-    <img src={HomeImage} alt="Home" width="100%" height="500px" 
-    />
+  Container,
+  Row,
+  Col,
+  Button,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+} from "reactstrap";
+const Home = () => {
+  return (
+    <div>
+      <img src={HomeImage} alt="Home" width="100%" height="500px" />
 
-    <div className="container">
-        <Row style={{"textAlign":"center", "margin":"40px"}} >
-            <Col   >
-            <Card   >
-            <CardBody  style={{"backgroundColor":"#e8baba"}}>
-          <CardTitle>Bussiness Events</CardTitle>
-          
-        </CardBody>
-        <CardImg top width="100%" src={Home1} alt="Card image cap" />
-   
-      </Card>
-            </Col>
+      <div className="container">
+        <Row style={{ textAlign: "center", margin: "40px" }}>
+          <Col>
+            <Card>
+              <CardBody style={{ backgroundColor: "#e8baba" }}>
+                <CardTitle>Bussiness Events</CardTitle>
+              </CardBody>
+              <CardImg top width="100%" src={Home1} alt="Card image cap" />
+            </Card>
+          </Col>
 
-            <Col >
-            <Card    >
-            <CardBody  style={{"backgroundColor":"#e8baba"}}>
-          <CardTitle>Weddings</CardTitle>
-        </CardBody>
-        <CardImg top width="100%" src={Home3} alt="Card image cap" />
-       
-      </Card>
-            </Col>
+          <Col>
+            <Card>
+              <CardBody style={{ backgroundColor: "#e8baba" }}>
+                <CardTitle>Weddings</CardTitle>
+              </CardBody>
+              <CardImg top width="100%" src={Home3} alt="Card image cap" />
+            </Card>
+          </Col>
 
-            <Col >
-            <Card   >
-            <CardBody  style={{"backgroundColor":"#e8baba"}}>
-          <CardTitle>Birthday Parties</CardTitle>
-        </CardBody>
-        <CardImg top width="100%" src={Birthday} alt="Card image cap" />
-    
-      </Card>
-            </Col>
+          <Col>
+            <Card>
+              <CardBody style={{ backgroundColor: "#e8baba" }}>
+                <CardTitle>Birthday Parties</CardTitle>
+              </CardBody>
+              <CardImg top width="100%" src={Birthday} alt="Card image cap" />
+            </Card>
+          </Col>
         </Row>
+      </div>
     </div>
-    </div>
-
-   
-  
-    )
-}
+  );
+};
+export default Home;
--- EventsAgencyClient/src/components/UserDashboard.js
+++ EventsAgencyClient/src/components/UserDashboard.js
@@ -1,8 +1,18 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import events from "../../redux/apis/events";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+  } from "reactstrap";
 
-
-
+  import { Table } from "reactstrap";
+  import { Link } from "react-router-dom";
 class UserDashboard extends React.Component{
 
     render(){
